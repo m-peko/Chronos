2820a90b2d466eadf499d9724040639a4701c91d
Marin Peko <mpeko00@fesb.hr>
2017-12-16 22:51:43 +0100

    Beginning of new task modal body and schedule solution

--- src/components/schedule/Schedule.jsx
+++ src/components/schedule/Schedule.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import ScheduleHeader from './ScheduleHeader';
+import ScheduleBody from './ScheduleBody';
 
 import '../../styles/components/schedule/schedule.css';
 
@@ -14,150 +16,8 @@ class Schedule extends Component {
         return (
             <main className='item main'>
                 <table>
-                    <thead>
-                        <tr>
-                            <th width='4%'>&nbsp;</th>
-                            <th width='12%'>Monday</th>
-                            <th width='12%'>Tuesday</th>
-                            <th width='12%'>Wednesday</th>
-                            <th width='12%'>Thursday</th>
-                            <th width='12%'>Friday</th>
-                            <th width='12%'>Saturday</th>
-                            <th width='12%'>Sunday</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>7AM</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>8AM</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>9AM</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>10AM</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>11AM</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>12AM</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>1PM</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>2PM</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>3PM</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>4PM</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>5PM</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>6PM</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>7PM</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                    </tbody>
+                    <ScheduleHeader/>
+                    <ScheduleBody/>
                 </table>
             </main>
         );
---
+++ src/components/schedule/ScheduleHeader.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const ScheduleHeader = (props) => {
+    return (
+        <thead>
+            <tr>
+                <th width='4%'>&nbsp;</th>
+                <th width='12%'>Monday</th>
+                <th width='12%'>Tuesday</th>
+                <th width='12%'>Wednesday</th>
+                <th width='12%'>Thursday</th>
+                <th width='12%'>Friday</th>
+                <th width='12%'>Saturday</th>
+                <th width='12%'>Sunday</th>
+            </tr>
+        </thead>
+    );
+};
+
+export default ScheduleHeader;
